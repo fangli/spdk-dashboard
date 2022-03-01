350721b0ae60a5857353d11b2ab7a3eb8377dde0
Fang Li <surivlee@gmail.com>
2022-03-01 00:15:03 -0800

    Added auto build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 rm -f spdk-dashboard
 
@@ -6,12 +6,14 @@ cd public
 npm install
 npm run build-dist
 cd ..
+
 rm -rf build
 
 mkdir -p build/linux-x86_64
 mkdir -p build/linux-arm64
 mkdir -p build/macos-x86_64
 
+go get
 GOOS=linux GOARCH=amd64 go build -o build/linux-x86_64/spdk_dashboard
 GOOS=linux GOARCH=arm64 go build -o build/linux-arm64/spdk_dashboard
 GOOS=darwin GOARCH=amd64 go build -o build/macos-x86_64/spdk_dashboard
@@ -19,4 +21,3 @@ GOOS=darwin GOARCH=amd64 go build -o build/macos-x86_64/spdk_dashboard
 echo "------------------"
 echo "The binary has been built to directory build/. Binary spdk_dashboard is the ONLY file that needs to be deployed."
 echo "Done."
-
